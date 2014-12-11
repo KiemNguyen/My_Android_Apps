61acb112186907b824eb02b1ee4d8e534818dc16
Kiem Nguyen <kiemnguyen@gmail.com>
2014-12-11 15:06:53 -0600

    Add Top10Songs app

---
+++ Top10Songs/src/net/kiemnguyen/top10songs/Application.java
@@ -0,0 +1,32 @@
+package net.kiemnguyen.top10songs;
+
+public class Application {
+	private String name;
+	private String artist;
+	private String releaseDate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public String toString() {
+		return "Name: " + this.name + "\n" +
+				"Artist: " + this.artist + "\n" +
+				"Released Date: " + this.releaseDate;
+	}	
+}
