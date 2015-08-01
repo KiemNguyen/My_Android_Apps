ffd49ad24548ab3b0ef690b19d0ae5b78b9bf136
Kiem Nguyen <kiemnguyen@gmail.com>
2015-08-01 00:05:56 -0500

    Removed the menu.

--- MyAppPortfolio/app/src/main/java/com/exvy/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/exvy/myappportfolio/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -83,18 +82,4 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
